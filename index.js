189eab65454e4e10defef1a3e0487bcb46c3806a
unknown <yeasin18801@gmail.com>
2022-11-16 00:29:33 +0600

    first external js file

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 
  
+ var a = 10;
+ var b = 20;
+ var c = 30;
+ // show to console
+ console.log(a)
+ // show to DOM (document object Model)
+ document.write(a)
+ // data pass by id 
+ // if(a < 10)
+ // {
+ //     console.log('false');
+ // }else{
+ //     console.log('true');
+ // }
 
+ // const d = (a < 10) ? 'number is less than 10':'number is greater than 10';
+ // console.log(d);
+
+ // tarnary operator 
+ console.log((b >=18) ? 'true' : 'false');
